9fa85ae71fb39c3ac03bbf91bb3c089fb1f35ac7
Pengsha Ying <yingfusheng@foxmail.com>
2022-01-14 17:41:04 +0800

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+/**
+ * @type {import('eslint-define-config').EslintConfig}
+ */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard',
+    'plugin:json/recommended',
+    'plugin:markdown/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+    // ts will check these rule
+    'no-undef': 'off',
+    'no-unused-vars': 'off',
+
+    // replace 'no-redeclare' with @typescript-eslint
+    'no-redeclare': 'off',
+    '@typescript-eslint/no-redeclare': ['error']
+  }
+}
